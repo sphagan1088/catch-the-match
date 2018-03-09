64ca6aed87e642bb8278e59df7e3c5d7807d9b25
Sean Hagan <sphagan1088@yahoo.com>
2018-03-08 18:20:23 -0800

    saving

--- app_client/src/components/Nav/Nav.js
+++ app_client/src/components/Nav/Nav.js
@@ -6,13 +6,12 @@ import "./Nav.css";
          return (
              <div>
                  <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-                    <a className="navbar-brand" >
-                    <img src="./images/trophy-icon3.png" />
-                    
+                    <a className="navbar-brand"> 
+                
+                    <img className="trophy" src="./images/trophy-icon3.png" />
+                   
                     </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
