7ab3ddb4be3c03799c7ff5eecda51abc4b94c3e6
Sean Hagan <sphagan1088@yahoo.com>
2018-03-08 19:59:42 -0800

    created the footer

--- app_client/src/App.js
+++ app_client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from "./components/Nav/Nav.js";
 import Title from "./components/Title/Title.js";
+import Footer from "./components/Footer/Footer.js";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className = "App">
         <Nav />
         <Title />
+        <Footer />
       </div>
     );
   }
